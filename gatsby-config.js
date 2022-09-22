698fd7222fdc67a808b16ad7c681e0a749d5bc90
sreak <shakilmith2@gmail.com>
2022-09-22 23:35:16 +0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: `My Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": ""
+    }
+  }, {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }]
+};
