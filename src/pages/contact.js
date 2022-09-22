dde16c84cb0802c3b8b9848b086fe6ed2be411ff
sreak <shakilmith2@gmail.com>
2022-09-23 00:37:15 +0600

    added

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import * as  React from 'react'
+
+const ContactPage=()=>{
+  return(
+    <div>
+        <h2>Hi, let's contact with me if you find any...isue 
+            here.
+        </h2>
+    </div>
+  )
+}
